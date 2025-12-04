995bd0fe1a99d690146feb5a7ae360b7db52882f
AIT-AS <a.schuetz79@gmail.com>
2025-12-04 15:56:53 +0100

    Lesson 06: Added TypeScript basics (string, number, boolean, arrays, tuples, functions, interfaces, union, type)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+// Lessons
+import Lesson_06 from "./lessons/Lesson_06/Lesson_06";
+
+// Homeworks
+
+
+
+function App() {
+  return (
+  <>
+  <Lesson_06 />
+  </>
+  );
+}
+
+export default App;
