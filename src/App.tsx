a5c3e89b2724f01fb16ede071c965a8dac01321f
AIT-AS <a.schuetz79@gmail.com>
2025-12-16 18:27:44 +0100

    Lesson_10

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import GlobalStyles from "styles/GlobalStyles";
 // import Lesson_06 from "lessons/Lesson_06/Lesson_06";
 // import Lesson_07 from "lessons/Lesson_07/Lesson_07";
 // import Lesson_08 from "lessons/Lesson_08/Lesson_08";
-import Lesson_09 from "lessons/Lesson_09/Lesson_09";
+// import Lesson_09 from "lessons/Lesson_09/Lesson_09";
+import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 
 
 // Homeworks
@@ -19,7 +20,8 @@ function App() {
       {/* <Lesson_07 /> */}
       <GlobalStyles />
       {/* <Lesson_08 /> */}
-      <Lesson_09 />
+      {/* <Lesson_09 /> */}
+      <Lesson_10 />
 
       {/* <Homework_06 /> */}
       {/* <Homework_07 /> */}
