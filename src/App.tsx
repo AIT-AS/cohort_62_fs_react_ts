2375d773be6c47526b8756ea9f951ebd4c8ec3cf
AIT-AS <a.schuetz79@gmail.com>
2025-12-21 13:15:49 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+
 import GlobalStyles from "styles/GlobalStyles";
 
 // Lessons
@@ -7,9 +8,7 @@ import GlobalStyles from "styles/GlobalStyles";
 // import Lesson_09 from "lessons/Lesson_09/Lesson_09";
 // import Lesson_10 from "lessons/Lesson_10/Lesson_10";
 // import Lesson_11 from "lessons/Lesson_11/Lesson_11";
-import Lesson_12 from "lessons/Lesson_12/Lesson_12";
-
-
+// import Lesson_12 from "lessons/Lesson_12/Lesson_12";
 
 // Homeworks
 // import Homework_06 from "homeworks/Homework_06/Homework_06";
@@ -18,7 +17,6 @@ import Lesson_12 from "lessons/Lesson_12/Lesson_12";
 // import Homework_09 from "homeworks/Homework_09/Homework_09";
 
 // Consultations
-
 
 function App() {
   return (
@@ -30,7 +28,7 @@ function App() {
       {/* <Lesson_09 /> */}
       {/* <Lesson_10 /> */}
       {/* <Lesson_11 /> */}
-      <Lesson_12 />
+      {/* <Lesson_12 /> */}
 
       {/* <Homework_06 /> */}
       {/* <Homework_07 /> */}
