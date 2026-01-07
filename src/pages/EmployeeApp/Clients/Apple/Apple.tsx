cb1225275eb871febbfc1189fcfc33e9dcac5971
AIT-AS <a.schuetz79@gmail.com>
2026-01-07 19:40:56 +0100

    Homework13

---
+++ src/pages/EmployeeApp/Clients/Apple/Apple.tsx
@@ -0,0 +1,12 @@
+import ClientTemplate from "../components/ClientTemplate/ClientTemplete";
+
+function Apple() {
+  return (
+    <ClientTemplate>
+      Apple Inc. is an American multinational corporation and technology company
+      headquartered in Cupertino, California, in Silicon Valley.
+    </ClientTemplate>
+  );
+}
+
+export default Apple;
